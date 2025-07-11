da1208958e0c241c5524e6ccfb580aee089357c0
Vishal <vishalsinghrao140@gmail.com>
2025-07-12 02:09:15 +0530

    project completed

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from '../components/Card'
+import Details from '../components/details'
+
+
+const Home = () => {
+  return (
+    <div>
+    <Card/>
+    <Details/>
+    </div>
+  )
+}
+
+export default Home
